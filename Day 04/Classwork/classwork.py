c2f705618c4360860c9f860b8c6bcde929e3656a
ditoghubeladze <ditogubeladze615@gmail.com>
2024-07-16 20:43:25 +0400

    fourth commit

---
+++ Day 04/Classwork/classwork.py
@@ -0,0 +1,32 @@
+# დავალება 1
+Favorite_Movie="Harry Potter"
+print(Favorite_Movie)
+
+#დავალება 2
+Classwork_time=10
+print(Classwork_time)
+
+#დავალება 3
+print(2+2)
+print(7*2)
+print(12/3)
+print(16//4)
+
+#დავალება 4
+
+# შეცდომაა city-ის მაგივრად City მიწერია
+city="London"
+print(City)
+
+#დავალება 5
+
+# My age
+age=15
+
+#my name
+name="dito"
+
+#printing code
+print("i am"+ name+ age+ "years old")
+
+
