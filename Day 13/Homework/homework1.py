942e3fcdccd93b84f9a10034adebe20d02042a41
ditoghubeladze <ditogubeladze615@gmail.com>
2024-09-10 15:13:43 +0400

    Thirtheenth commit

---
+++ Day 13/Homework/homework1.py
@@ -0,0 +1,21 @@
+# 1. for loop-ის მეშვეობით გამოიტანეთ რიცხვი 10, 7-ჯერ.
+
+
+for i in range(7):
+    print(10)
+
+
+# 2. for loop-ის მეშვეობით გამოიტანეთ 'hello' 8-ჯერ.
+
+for i in range(8):
+    print("Hello")
+
+
+# 3. Input()-ის გამოყენებით მომხმარებელს შემოატანინეთ მისი სახელი,
+# შემდეგ for loop-ით კი დაპრინტეთ ("hello" + მომხმარებლის სახელი) 5 ჯერ.
+
+user_name = input("Enter your name: ")
+
+for i in range(5):
+    print(f"Hello {user_name}")
+    
---
+++ Day 13/Homework/homework2.py
@@ -0,0 +1,26 @@
+# 4. while loop-ის მეშვეობით გამოიტანეთ ციფრები 10-დან 30-მდე.
+
+num = 10
+
+while num <= 30:
+    print(num)
+    num= num+1
+
+
+
+# 5. while loop-ის მეშვეობით გამოიტანეთ ციფრები 50-დან 10-მდე.
+
+num = 50
+
+while num >= 10:
+    print(num)
+    num= num-1
+
+
+# 6. while loop-ის მეშვეობით გამოიტანეთ 'goa best' 10-ჯერ.
+
+i = 1
+
+while i <= 10:
+    print("Goa Best")
+    i= i + 1
