3ca6033c3776b5e6c13b9dd310f6de74339c0826
ditoghubeladze <ditogubeladze615@gmail.com>
2024-11-13 23:27:05 +0400

    commit

---
+++ Day 23/Classwork/classwork2.py
@@ -0,0 +1,7 @@
+# 2. ჩამოწერე უკვე ნასწავლი ფუნქციის 3-4 მაგალითი.
+
+print("Batoni Sandro")
+
+type("integer")
+
+input("hello")
---
+++ Day 23/Classwork/classwork3.py
@@ -0,0 +1,5 @@
+# 3. შექმენი for loop(), რომელიც 5-ჯერ გამოიტანს 'hello'-ს.
+
+
+for i in range(5):
+    print('hello')
